06560b52b89b09717f871c23ca8a45a1bb9742b8
omaro <omaroubari16@gmail.com>
2025-03-10 13:24:43 +0300

    x

--- nextjs/src/ui/modules/LogoList.tsx
+++ nextjs/src/ui/modules/LogoList.tsx
@@ -30,36 +30,46 @@ export default async function LogoList({
 
 	return (
 		<section className="">
-			<div className="section relative mx-auto max-w-7xl space-y-8 border-x border-b border-neutral-400 py-8 lg:py-16">
+			<div className="section mx-auto max-w-7xl space-y-8 border-x border-b border-neutral-400 px-0 pt-8 pb-0 lg:pt-16">
 				{(pretitle || intro) && (
 					<header className="richtext mx-auto max-w-screen-sm text-center text-balance">
 						<Pretitle>{pretitle}</Pretitle>
 						<PortableText value={intro} />
 					</header>
 				)}
-				{/* <AnimatedGroup
-					className="grid grid-cols-2 divide-x divide-neutral-400 *:p-8 md:grid-cols-3 lg:grid-cols-4"
-					preset="scale"
-				>
-					{allLogos.map((logo, key) => (
-						<Img
-							className="h-[2.5em] w-[200px] shrink-0 object-contain max-sm:w-[150px]"
-							style={{ '--index': key } as React.CSSProperties}
-							image={logo.image?.[logoType] || logo.image?.default}
-							width={400}
-							alt={logo.name}
-							key={key}
-						/>
-					))}
-				</AnimatedGroup> */}
-				<div className="absolute bottom-8 grid grid-cols-2 divide-x divide-neutral-400 *:p-8 md:grid-cols-3 lg:bottom-16 lg:grid-cols-4">
-					{allLogos.map((logo, key) => (
+				<div className="relative">
+					<AnimatedGroup
+						className="grid grid-cols-2 items-center justify-center *:p-8 md:grid-cols-3 lg:grid-cols-4"
+						preset="scale"
+					>
+						{allLogos.map((logo, key) => (
+							<Img
+								className="mx-auto h-[2.5em] w-[200px] shrink-0 object-contain grayscale hover:grayscale-0 max-sm:w-[150px]"
+								style={{ '--index': key } as React.CSSProperties}
+								image={logo.image?.[logoType] || logo.image?.default}
+								width={400}
+								alt={logo.name}
+								key={key}
+							/>
+						))}
+					</AnimatedGroup>
+					<div
+						aria-hidden="true"
+						className="pointer-events-none absolute bottom-0 grid w-full grid-cols-2 divide-x divide-neutral-400 border-t border-neutral-400 md:grid-cols-3 lg:grid-cols-4"
+					>
+						{allLogos.map((logo, key) => (
+							<div
+								className="h-[104px] w-full shrink-0 object-contain max-sm:w-[150px]"
+								style={{ '--index': key } as React.CSSProperties}
+								key={key}
+							></div>
+						))}
 						<div
-							className="h-[2.5em] w-[200px] shrink-0 object-contain max-sm:w-[150px]"
-							style={{ '--index': key } as React.CSSProperties}
-							key={key}
+							className="h-[104px] w-full shrink-0 object-contain max-sm:w-[150px]"
+							style={{ '--index': allLogos.length + 1 } as React.CSSProperties}
+							// key={key}
 						></div>
-					))}
+					</div>
 				</div>
 			</div>
 		</section>
